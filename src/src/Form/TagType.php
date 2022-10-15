706707cfde3181879c0298508e1dab0ddbd881b3
Adrien Albuquerque <adrialb95@gmail.com>
2022-10-16 01:15:48 +0200

    Create/Edit/Delete Tag

--- src/src/Form/TagType.php
+++ src/src/Form/TagType.php
@@ -12,9 +12,7 @@ class TagType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('posts')
-        ;
+            ->add('name');
     }
 
     public function configureOptions(OptionsResolver $resolver): void
