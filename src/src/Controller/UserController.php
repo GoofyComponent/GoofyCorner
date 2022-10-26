78154def53e0dc25bb7cd19bfdcdf4f1e9f1bcde
ThomAzgo <thomas.buisson@hetic.net>
2022-10-26 09:38:37 +0200

    edit form

--- src/src/Controller/UserController.php
+++ src/src/Controller/UserController.php
@@ -2,30 +2,61 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+// Edit User Form
+use App\Security\EmailVerifier;
+use App\Form\RegistrationFormType;
+use App\Repository\UserRepository;
+use Symfony\Component\Mime\Address;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\User;
-use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+use App\Security\AppCustomAuthenticator;
+// Published Posts
+use App\Entity\Post;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+use App\Form\EditUserType;
 
 class UserController extends AbstractController
 {
-    #[Route('/user/{id}', name:'app_user', methods:['GET'])]
-    public function index($id, UserRepository $userRepository): Response
+    #[Route('/user/edit', name:'app_user_edit')]
+    public function index(UserRepository $userRepository, Request $request): Response
     {
-        $userFetch = $userRepository->find($id);
         $user = $this->getUser();
-
-        if($userFetch->getId() == $user->getId()) {
-            $canEdit = true;
-        } else {
-            $canEdit = false;
+        if (!$user){
+            return $this->redirectToRoute('app_home');
         }
 
+        $form = $this->createForm(EditUserType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Redirect
+            $task = $form->getData();
+            // Set and Hash password
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+            // Save user data and Flush
+            $entityManager->persist($user);
+            $entityManager->flush();
+            // Log in modified user
+            $userAuthenticator->authenticateUser($user, $appCustomAuthenticator, $request);
+        }
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
-            'user' => $user,
-            'canEdit' => $canEdit,
+            'editForm'=>$form->createView(),
+            'user' => $user
         ]);
     }
 }
